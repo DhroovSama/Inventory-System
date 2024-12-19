9b3197a22c3cda8e1b1ca4a69c88c371d19ab59b
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2024-12-19 20:46:41 +0530

    Reapply "Basic Inventory System v.10 Implemented"
    This reverts commit 06c160dea4cc05562fca8f6114975180ae353877.

---
+++ Assets/Scripts/Inventory System/Scripts/InventorySlot.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Inventory.sys
+{
+    [System.Serializable]   
+    public class InventorySlot 
+    {
+        public ItemData itemData;
+        public int quantity;
+
+        public InventorySlot(ItemData id,int q)
+        {
+            itemData = id;
+            quantity = q;
+        }
+
+        public bool IsEmpty()
+        {
+            return itemData == null || quantity <= 0;
+        }
+
+        public void ClearSlot()
+        {
+            itemData = null;
+            quantity = 0;
+        }
+
+        public void SetItem(ItemData newItemData, int newQuantity)
+        {
+            itemData = newItemData;
+            quantity = newQuantity;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Inventory System/Scripts/ItemType.cs
@@ -0,0 +1,11 @@
+namespace Inventory.sys
+{
+    public enum ItemType
+    {
+        //Item types 
+        Weapon,
+        Consummable,
+        Coin,
+        PowerUp
+    }
+}
